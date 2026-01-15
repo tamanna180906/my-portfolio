56bb0ffbda9ddb56fa3eba05f97fa82cd7b2ee0e
tamanna180906 <myeuphoria330@gmail.com>
2026-01-16 00:37:11 +0600

    about skill

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import html from "../assets/html.jpg"
 import Container from './Container'
+import { FaBootstrap, FaCss3Alt, FaHtml5, FaJs, FaReact } from 'react-icons/fa'
+import { SiTailwindcss } from 'react-icons/si'
 
 function About() {
     return (
@@ -15,10 +17,37 @@ function About() {
                         smoothly across different devices and screen sizes.
                     </p>
                 </div>
-                <div className='pt-[100px]' >
-                    <div>
-                        <div>
-                            <img className='h-[50px] w-[50px]' src={html}></img>
+                <div className='pt-[100px]'>
+                    <p className='font-bold font-rc text-center text-[26px]'>Skills & Technologies</p>
+                    <div className='pt-[30px] flex justify-center gap-8 flex-wrap'>
+                        <div className="flex items-center gap-2">
+                            <FaHtml5 className="text-orange-500 text-2xl" />
+                            <span>HTML</span>
+                        </div>
+
+                        <div className="flex items-center gap-2">
+                            <FaCss3Alt className="text-blue-500 text-2xl" />
+                            <span>CSS</span>
+                        </div>
+
+                        <div className="flex items-center gap-2">
+                            <SiTailwindcss className="text-sky-400 text-2xl" />
+                            <span>Tailwind</span>
+                        </div>
+
+                        <div className="flex items-center gap-2">
+                            <FaJs className="text-yellow-400 text-2xl" />
+                            <span>JavaScript</span>
+                        </div>
+
+                        <div className="flex items-center gap-2">
+                            <FaReact className="text-cyan-400 text-2xl" />
+                            <span>React</span>
+                        </div>
+
+                        <div className="flex items-center gap-2">
+                            <FaBootstrap className="text-purple-600 text-2xl" />
+                            <span>Bootstrap</span>
                         </div>
                     </div>
                 </div>

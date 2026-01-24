eef0c5ea2a692775d4f79c696244d2a6f1d77dd5
tamanna180906 <myeuphoria330@gmail.com>
2026-01-24 23:52:01 +0600

    responsive banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -4,18 +4,20 @@ import pro3 from "../assets/pro3.jpg"
 
 function Banner() {
   return (
-    <div className='bg-gray-100 pt-[200px] pb-[100px]'>
+    <div className='bg-gray-100 pt-[120px] md:pt-[150px] lg:pt-[200px] pb-[50px] md:pb-[75px] lg:pb-[100px]'>
       <Container>
         <div className='flex justify-between'>
-          <div className='w-[70%] pt-10'>
-            <h2 className='font-rc text-[50px] text-gray-800'>Hello, I’m </h2>
-            <h2 className='font-rc text-[50px] text-gray-800'>Tamanna Hasan Navila </h2>
-            <h2 className='font-rc text-[50px] font-bold text-gray-800'>Frontend Web Developer  </h2>
-            <p className='pr-[100px] pt-5 font-rc text-[20px] text-gray-800'>I create websites that are visually clean, fully 
+          <div className='lg:w-[70%] md:w-[70%] w-full lg:pt-10 pt-5 text-center md:text-start lg:text-start'>
+            <h2 className='font-rc text-[24px] md:text-[30px] lg:text-[50px] text-gray-800'>Hello, I’m </h2>
+            <h2 className='font-rc text-[24px] md:text-[30px] lg:text-[50px] text-gray-800'>Tamanna Hasan Navila </h2>
+            <h2 className='font-rc text-[28px] md:text-[36px] lg:text-[50px] font-bold text-gray-800'>Frontend Web Developer  </h2>
+            <p className='lg:pt-5 pt-2 font-rc text-[16px] md:text-[18px] lg:text-[20px] text-gray-800'>I create websites that are visually clean, fully
               responsive, and user-friendly, aiming to provide a simple and smooth experience for everyone who visits.</p>
           </div>
-          <div className='h-[400px] w-[400px] bg-amber-200 rounded-full flex items-center justify-center overflow-hidden'>
-            <img className='object-cover h-full w-full' src={pro3}></img>
+          <div className='mt-8 lg:mt-0 flex justify-center lg:justify-end'>
+            <div className='md:h-[250px] md:w-[250px] lg:h-[400px] lg:w-[400px] bg-amber-200 rounded-full overflow-hidden lg:block md:block hidden'>
+              <img className='object-cover h-full w-full' src={pro3} alt="Tamanna" />
+            </div>
           </div>
         </div>
       </Container>

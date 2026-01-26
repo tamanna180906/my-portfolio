43dbba73d18c280a6b436a97f717aebee48b1562
tamanna180906 <myeuphoria330@gmail.com>
2026-01-26 19:05:12 +0600

    responsive about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,20 +6,20 @@ import { SiTailwindcss } from 'react-icons/si'
 
 function About() {
     return (
-        <div className='py-[150px]'>
+        <div className='lg-py-[150px] md:py-[100px] py-[50px]'>
             <Container>
-                <div className='w-[70%] mx-auto '>
-                    <h2 className='text-center text-[50px] font-rc font-bold'>About Me</h2>
-                    <p className='text-center text-[20px] font-rc '>
+                <div className='lg:w-[70%] md:w-[80%] w-full mx-auto'>
+                    <h2 className='text-center text-[24px] md:text-[30px] lg:text-[50px] font-rc font-bold'>About Me</h2>
+                    <p className='text-center text-[16px] md:text-[18px] lg:text-[20px] font-rc '>
                         I’m a Frontend Developer with experience in creating clean and responsive web interfaces. I
                         focus on writing structured, maintainable code and building designs that are simple, intuitive,
                         and easy to use. I enjoy translating ideas and designs into functional web experiences that work
                         smoothly across different devices and screen sizes.
                     </p>
                 </div>
-                <div className='pt-[100px]'>
-                    <p className='font-bold font-rc text-center text-[26px]'>Skills & Technologies</p>
-                    <div className='pt-[30px] flex justify-center gap-8 flex-wrap'>
+                <div className='lg-pt-[100px] md:pt-[75px] pt-[50px] '>
+                    <p className='font-bold font-rc text-center lg:text-[26px] md:text-[22px] text-[18px]'>Skills & Technologies</p>
+                    <div className='lg:pt-[30px] md:pt-[20px] pt-[10px] flex justify-center gap-x-8 gap-y-4 flex-wrap'>
                         <div className="flex items-center gap-2">
                             <FaHtml5 className="text-orange-500 text-2xl" />
                             <span>HTML</span>
